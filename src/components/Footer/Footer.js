2867c93f45bcdb252f9783c2d8f9ba2732f086b5
Sean Dodson <dodsonsean@comcast.net>
2021-11-09 20:47:15 -0500

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,61 @@
 import React from 'react';
 import * as styles from './Footer.module.scss';
+import linkedinImage from '../../images/linkedin.png';
+import youtubeImage from '../../images/youtube.png';
+import githubImage from '../../images/github.png';
+import { Link } from 'gatsby';
 
-const Footer = () => <div className={styles.footer}></div>;
+const LINKEDIN_URL = 'https://www.linkedin.com/in/sean-dodson21';
+const YOUTUBE_URL = 'https://www.youtube.com/channel/UC7X9mQ_XtTYWzr9Tf_NYcIg';
+const GITHUB_URL = 'https://github.com/sdodson99';
+
+const Footer = () => (
+  <div className={styles.footer}>
+    <div className="content-container">
+      <div className="flex flex-col items-center text-center sm:flex-row sm:justify-between sm:items-start">
+        <div className="sm:flex">
+          <Link className={styles.navLink} to="/">
+            Home
+          </Link>
+          <Link className={styles.navLink} to="/personal">
+            Personal
+          </Link>
+          <Link className={styles.navLink} to="/career">
+            Career
+          </Link>
+          <Link className={styles.navLink} to="/contact">
+            Contact
+          </Link>
+        </div>
+        <div className="mt-8 sm:mt-0">
+          <div>Follow My Socials</div>
+          <div className="flex">
+            <a href={LINKEDIN_URL} target="_blank" rel="noreferrer">
+              <img
+                className={styles.socialImage}
+                src={linkedinImage}
+                alt="LinkedIn"
+              />
+            </a>
+            <a href={YOUTUBE_URL} target="_blank" rel="noreferrer">
+              <img
+                className={styles.socialImage}
+                src={youtubeImage}
+                alt="YouTube"
+              />
+            </a>
+            <a href={GITHUB_URL} target="_blank" rel="noreferrer">
+              <img
+                className={styles.socialImage}
+                src={githubImage}
+                alt="GitHub"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Footer;
