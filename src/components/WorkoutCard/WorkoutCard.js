7f290e4c8619b50780657c70379e5ed687ef69f3
Sean Dodson <dodsonsean@comcast.net>
2021-11-14 19:21:33 -0500

    Format workout data

--- src/components/WorkoutCard/WorkoutCard.js
+++ src/components/WorkoutCard/WorkoutCard.js
@@ -2,12 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as styles from './WorkoutCard.module.scss';
 
-const WorkoutCard = ({ date, exercises }) => (
-  <div className={styles.workoutCard}>
-    {date}
-    <pre>{JSON.stringify(exercises, null, 4)}</pre>
-  </div>
-);
+const WorkoutCard = ({ date, exercises }) => {
+  const dateDisplay = new Date(date).toLocaleDateString();
+
+  return (
+    <div className={styles.workoutCard}>
+      <h2 className="text-2xl">{dateDisplay} Workout</h2>
+      <div className="mt-5">
+        {exercises.map((e) => (
+          <div key={e.name} className="pb-3 flex">
+            <h3 className="flex-1 text-lg">{e.name}</h3>
+            <table className="table-auto flex-1">
+              <thead>
+                <tr>
+                  <th>Set</th>
+                  <th>Reps</th>
+                  <th>Weight (lbs.)</th>
+                </tr>
+              </thead>
+              <tbody>
+                {e.sets.map((s) => (
+                  <tr key={`${e.name}_${s.set}`} className="text-center">
+                    <td>{s.set}</td>
+                    <td>{s.reps}</td>
+                    <td>{s.weight}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 WorkoutCard.propTypes = {
   date: PropTypes.number.isRequired,
