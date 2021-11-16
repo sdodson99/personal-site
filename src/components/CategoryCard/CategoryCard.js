391c753b8c811f8adbbe573b8e0fa76c4a307c5b
Sean Dodson <dodsonsean@comcast.net>
2021-11-15 21:17:12 -0500

    Fix category card order

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -5,15 +5,15 @@ import { Link } from 'gatsby';
 
 const CategoryCard = ({ title, description, imageSrc, to }) => (
   <div className={styles.categoryCard}>
-    <div className="text-center sm:text-left sm:flex sm:flex-col sm:items-start">
+    <div className="flex order-1 justify-center sm:order-2">
+      <img className={styles.categoryImage} src={imageSrc} alt={title} />
+    </div>
+    <div className="mt-5 order-2 text-center sm:text-left sm:flex sm:flex-col sm:items-start sm:order-1 sm:mt-0">
       <h2 className="text-2xl">{title}</h2>
       <p className="mt-5 font-thin">{description}</p>
       <Link className="mt-5 btn btn-primary" to={to}>
         Learn More
       </Link>
-    </div>
-    <div className="flex justify-center">
-      <img className={styles.categoryImage} src={imageSrc} alt={title} />
     </div>
   </div>
 );
