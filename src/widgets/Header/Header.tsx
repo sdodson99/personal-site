d5e807dd89e479068732e80d7b109423459e8db7
Sean Dodson <dodsonsean@comcast.net>
2022-09-17 14:07:06 -0400

    SD-14: Add home link to header logo

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './Header.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type HeaderProps = {};
 
 export const Header = ({}: HeaderProps) => (
   <header className={styles.header} data-testid="Header">
-    <Image src="/logo.svg" alt="Sean Dodson Logo" width={50} height={50} />
+    <Link href="/">
+      <a>
+        <Image src="/logo.svg" alt="Sean Dodson Logo" width={50} height={50} />
+      </a>
+    </Link>
   </header>
 );
