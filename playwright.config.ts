af2553654da21d83e4e4b7c2764e69aea27f8d22
Sean Dodson <dodsonsean@comcast.net>
2022-10-15 14:09:31 -0400

    SD-62: Add Playwright screenshot difference ratio

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './test/e2e',
+  retries: 1,
+  use: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    headless: true,
+  },
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.2,
+    },
+  },
+};
+
+module.exports = config;
