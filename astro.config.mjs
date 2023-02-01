1dcb57c77b047bf9bba72c5540f66baccb73c512
Sean Dodson <dodsonsean@comcast.net>
2023-02-01 13:28:10 -0500

    Add image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
 
 export default defineConfig({
   site: 'https://seandodson.com',
-  integrations: [tailwind(), prefetch(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), prefetch(), robotsTxt(), sitemap(), image()],
   markdown: {
     shikiConfig: {
       theme: 'slack-dark',
