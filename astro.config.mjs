c6d0f984fe55c3b98f42f35e4410a6404e3c654e
Sean Dodson <dodsonsean@comcast.net>
2023-02-01 12:48:18 -0500

    Upgrade to Astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import partytown from '@astrojs/partytown';
 import prefetch from '@astrojs/prefetch';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://seandodson.com',
-  integrations: [tailwind(), partytown(), prefetch(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), prefetch(), robotsTxt(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'slack-dark',
